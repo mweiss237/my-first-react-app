717db539b2d5873b577db41cb49e34445dbead99
Moritz <moritz.weiss95@hotmail.de>
2021-11-11 15:56:14 +0100

    use firebase Database to store new Meetups

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,49 @@
+import { useRef } from "react";
 import Card from "../ui/Card"
 import styles from "./NewMeetupForm.module.css"
 
 export default function NewMeetupForm(props) {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+        
+        const meetupData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            desciption: enteredDescription
+        }
+
+        props.onAddMeetup(meetupData)
+    }
 
     return <Card>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={submitHandler}>
             <div className={styles.control}>
                 <label htmlFor="title">Meetup Title</label>
-                <input type="text" required id="title" />
+                <input type="text" required id="title" ref={titleInputRef} />
             </div>
             <div className={styles.control}>
                 <label htmlFor="image">Meetup Image</label>
-                <input type="url" required id="image" />
+                <input type="url" required id="image" ref={imageInputRef} />
             </div>
             <div className={styles.control}>
                 <label htmlFor="address">Meetup Address</label>
-                <input type="text" required id="address" />
+                <input type="text" required id="address" ref={addressInputRef} />
             </div>
             <div className={styles.control}>
                 <label htmlFor="description">Meetup Description</label>
-                <textarea required id="description" rows='5' />
+                <textarea required id="description" ref={descriptionInputRef} rows='5' />
             </div>
             <div className={styles.actions}>
                 {/* Ein einzelner Button ohne type Attribut wird innerhalb einer Form automatisch als Submit Button erkannt. */}
@@ -27,5 +51,6 @@ export default function NewMeetupForm(props) {
             </div>
         </form>
     </Card>
+    
 
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,26 @@
+import { useHistory } from "react-router";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(props) {
+
+    const history = useHistory();
+
+    const addMeetupHandler = (meetupData) => {
+        fetch('https://my-first-react-app-aafa2-default-rtdb.europe-west1.firebasedatabase.app/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData), // JSON.stringify Pflicht!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            history.replace('/')
+        })
+    }
+
   return (
     <section>
       <h1>New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
   );
 }
